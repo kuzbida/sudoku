e00896e53c09b0f941b09b383e02975d4cfae850
kuzbida <kuzbida@gmail.com>
2015-09-30 23:51:50 +0300

    styling

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,15 @@
 angular.module('sudokuApp', [])
-    .controller('GameController', function() {
-        console.log('game init')
+    .controller('GameController', function($scope) {
+        $scope.numbers = [1,2,3,4,5,6,7,8,9];
+        $scope.square = {};
+        var ceil = function(row, col){
+            return '<input ng-model="square.'+row+'_'+col+'"/>';
+        };
+        //appending columns
+        for(var i = 0; i < 9; i++){
+            //appending rows
+            for(var j = 0; j < 9; j++){
+                $('.gameSquare').append(ceil(i,j))
+            }
+        }
     });
